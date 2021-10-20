208079cbce29fa289ea12548864ef5b38e32fa56
Yosra Houimli <houimliyosra.b@gmail.com>
2021-10-20 16:49:08 +0200

    Data JPA TEST

--- src/test/java/com/example/demo/student/StudentRepositoryTest.java
+++ src/test/java/com/example/demo/student/StudentRepositoryTest.java
@@ -1,4 +1,13 @@
+package com.example.demo.student;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class StudentRepositoryTest {
-  
+
+    @Test
+    void selectExistsEmail() {
+
+    }
 }
